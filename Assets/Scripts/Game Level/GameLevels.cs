f91d40b4c248cb4ccd2a0f3771c9f8eec293b8f4
Beatriz Antunes <157928671+Beatriz-Ceschin-Antunes@users.noreply.github.com>
2026-02-19 13:03:05 +0100

    feat: create game levels class

---
+++ Assets/Scripts/Game Level/GameLevels.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class GameLevels : MonoBehaviour
+{
+    public static GameLevels Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    private string level; // can be beginner, intermediate or advanced
+
+    public void SetLevel(string level)
+    {
+        this.level = level;
+    }
+
+    // Returns walk speed corresponding to level chosen by user.
+    public float GetWalkSpeed()
+    {
+        switch (level)
+        {
+            case "beginner": return 3f;
+                break;
+            case "intermediate": return 6f;
+                break;
+            case "advanced": return 9f;
+                break;
+            default: return 6f;
+        }
+    }
+
+    // Returns run speed corresponding to level chosen by user.
+    public float GetRunSpeed()
+    {
+        switch (level)
+        {
+            case "beginner":
+                return 9f;
+                break;
+            case "intermediate":
+                return 15f;
+                break;
+            case "advanced":
+                return 21f;
+                break;
+            default: return 15f;
+        }
+    }
+}
