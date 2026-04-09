2755d08576e579a3866eee861a1f53f63b59a3f6
Beatriz Antunes <157928671+Beatriz-Ceschin-Antunes@users.noreply.github.com>
2026-04-09 14:00:18 +0200

    feat: add TC018

---
+++ Assets/Tests/EditMode/TC018.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TC018
+{
+    //[Test]
+    //public void BeginnerLevel_ReturnsCorrectTrivia()
+    //{
+    //    // Set up object
+    //    GameObject obj = new GameObject();
+    //    GameLevels gameLevels = obj.AddComponent<GameLevels>();
+    //    TriviaBankSO easyTrivia = EasyTriviaBank;
+
+    //    // Check that correct values are returned
+    //    gameLevels.SetLevel("beginner");
+    //    TriviaBankSO returnedTrivia = gameLevels.GetTrivia();
+    //    Assert.AreEqual(easyTrivia, returnedTrivia);
+
+    //    Object.DestroyImmediate(obj);
+    //}
+}
