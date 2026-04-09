e5939f33863d755b69c6ec60a5fe4ef159a7c7be
Beatriz Antunes <157928671+Beatriz-Ceschin-Antunes@users.noreply.github.com>
2026-04-09 14:01:00 +0200

    feat: add TC024

---
+++ Assets/Tests/EditMode/TC024.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TC024
+{
+    //[Test]
+    //public void AdvancedLevel_ReturnsCorrectTrivia()
+    //{
+    //    // Set up object
+    //    GameObject obj = new GameObject();
+    //    GameLevels gameLevels = obj.AddComponent<GameLevels>();
+    //    TriviaBankSO advTrivia = AdvancedTriviaBank;
+
+    //    // Check that correct values are returned
+    //    gameLevels.SetLevel("advanced");
+    //    TriviaBankSO returnedTrivia = gameLevels.GetTrivia();
+    //    Assert.AreEqual(advTrivia, returnedTrivia);
+
+    //    Object.DestroyImmediate(obj);
+    //}
+}
