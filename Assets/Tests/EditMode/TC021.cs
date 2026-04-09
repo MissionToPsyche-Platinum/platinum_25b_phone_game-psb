8c4e791694f0af7c9492f0716748b359767bc03a
Beatriz Antunes <157928671+Beatriz-Ceschin-Antunes@users.noreply.github.com>
2026-04-09 14:00:41 +0200

    feat: add TC021

---
+++ Assets/Tests/EditMode/TC021.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TC021
+{
+    //[Test]
+    //public void IntermediateLevel_ReturnsCorrectTrivia()
+    //{
+    //    // Set up object
+    //    GameObject obj = new GameObject();
+    //    GameLevels gameLevels = obj.AddComponent<GameLevels>();
+    //    TriviaBankSO intTrivia = IntermediateTriviaBank;
+
+    //    // Check that correct values are returned
+    //    gameLevels.SetLevel("intermediate");
+    //    TriviaBankSO returnedTrivia = gameLevels.GetTrivia();
+    //    Assert.AreEqual(intTrivia, returnedTrivia);
+
+    //    Object.DestroyImmediate(obj);
+    //}
+}
